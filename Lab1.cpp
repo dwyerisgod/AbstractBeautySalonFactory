e9cf445acd1220764d9d589e1b939b4b44064793
dwyerisgod <kiril.turov.05@icloud.com>
2023-09-18 15:06:46 +0200

    - optimization & localization

--- Lab1.cpp
+++ Lab1.cpp
@@ -1,32 +1,57 @@
 #include <iostream>
+using namespace std;
 
-// Абстрактный класс для стрижки
+// Абстрактні класи для послуг
 class Haircut {
 public:
+    virtual ~Haircut() {}
     virtual void cutHair() = 0;
 };
 
-// Абстрактный класс для маникюра
 class Manicure {
 public:
+    virtual ~Manicure() {}
     virtual void doManicure() = 0;
 };
 
-// Абстрактный класс для педикюра
 class Pedicure {
 public:
+    virtual ~Pedicure() {}
     virtual void doPedicure() = 0;
 };
 
-// Абстрактная фабрика для салона красоты
+// Конкретні класи для послуги салону "Luxury Beauty"
+class LuxuryHaircut : public Haircut {
+public:
+    void cutHair() override {
+        cout << "Робимо розкішну стрижку" << endl;
+    }
+};
+
+class LuxuryManicure : public Manicure {
+public:
+    void doManicure() override {
+        cout << "Робимо розкішний манікюр" << endl;
+    }
+};
+
+class LuxuryPedicure : public Pedicure {
+public:
+    void doPedicure() override {
+        cout << "Робимо розкішний педикюр" << endl;
+    }
+};
+
+// Абстрактна фабрика
 class BeautySalonFactory {
 public:
+    virtual ~BeautySalonFactory() {}
     virtual Haircut* createHaircutService() = 0;
     virtual Manicure* createManicureService() = 0;
     virtual Pedicure* createPedicureService() = 0;
 };
 
-// Конкретная фабрика для салона красоты "Luxury Beauty"
+// Конкретна фабрика для салону краси "Luxury Beauty"
 class LuxuryBeautyFactory : public BeautySalonFactory {
 public:
     Haircut* createHaircutService() override {
@@ -42,7 +67,29 @@ public:
     }
 };
 
-// Конкретная фабрика для салона красоты "Economy Beauty"
+// Конкретні класи для послуги салону "Economy Beauty"
+class EconomyHaircut : public Haircut {
+public:
+    void cutHair() override {
+        cout << "Робимо економ стрижку" << endl;
+    }
+};
+
+class EconomyManicure : public Manicure {
+public:
+    void doManicure() override {
+        cout << "Робимо економ манікюр" << endl;
+    }
+};
+
+class EconomyPedicure : public Pedicure {
+public:
+    void doPedicure() override {
+        cout << "Робимо економ педикюр" << endl;
+    }
+};
+
+// Абстрактний клас фабрики для салону краси "Economy Beauty"
 class EconomyBeautyFactory : public BeautySalonFactory {
 public:
     Haircut* createHaircutService() override {
@@ -58,7 +105,29 @@ public:
     }
 };
 
-// Конкретная фабрика для салона красоты "Premium Beauty"
+// Конкретні класи для послуги салону "Premium Beauty"
+class PremiumHaircut : public Haircut {
+public:
+    void cutHair() override {
+        cout << "Робимо преміум стрижку" << endl;
+    }
+};
+
+class PremiumManicure : public Manicure {
+public:
+    void doManicure() override {
+        cout << "Робимо преміум манікюр" << endl;
+    }
+};
+
+class PremiumPedicure : public Pedicure {
+public:
+    void doPedicure() override {
+        cout << "Робимо преміум педикюр" << endl;
+    }
+};
+
+// Конкретні класи для послуги салону "Premium Beauty"
 class PremiumBeautyFactory : public BeautySalonFactory {
 public:
     Haircut* createHaircutService() override {
@@ -74,74 +143,8 @@ public:
     }
 };
 
-// Конкретные классы для услуг салона "Luxury Beauty"
-class LuxuryHaircut : public Haircut {
-public:
-    void cutHair() override {
-        std::cout << "Делаем роскошную стрижку" << std::endl;
-    }
-};
-
-class LuxuryManicure : public Manicure {
-public:
-    void doManicure() override {
-        std::cout << "Делаем роскошный маникюр" << std::endl;
-    }
-};
-
-class LuxuryPedicure : public Pedicure {
-public:
-    void doPedicure() override {
-        std::cout << "Делаем роскошный педикюр" << std::endl;
-    }
-};
-
-// Конкретные классы для услуг салона "Economy Beauty"
-class EconomyHaircut : public Haircut {
-public:
-    void cutHair() override {
-        std::cout << "Делаем экономичную стрижку" << std::endl;
-    }
-};
-
-class EconomyManicure : public Manicure {
-public:
-    void doManicure() override {
-        std::cout << "Делаем экономичный маникюр" << std::endl;
-    }
-};
-
-class EconomyPedicure : public Pedicure {
-public:
-    void doPedicure() override {
-        std::cout << "Делаем экономичный педикюр" << std::endl;
-    }
-};
-
-// Конкретные классы для услуг салона "Premium Beauty"
-class PremiumHaircut : public Haircut {
-public:
-    void cutHair() override {
-        std::cout << "Делаем премиум стрижку" << std::endl;
-    }
-};
-
-class PremiumManicure : public Manicure {
-public:
-    void doManicure() override {
-        std::cout << "Делаем премиум маникюр" << std::endl;
-    }
-};
-
-class PremiumPedicure : public Pedicure {
-public:
-    void doPedicure() override {
-        std::cout << "Делаем премиум педикюр" << std::endl;
-    }
-};
-
 int main() {
-    std::string salonType = "luxury"; // или "economy" или "premium"
+    string salonType = "luxury"; // або "economy" або "premium"
     BeautySalonFactory* salonFactory = nullptr;
 
     if (salonType == "luxury") {
@@ -151,7 +154,7 @@ int main() {
     } else if (salonType == "premium") {
         salonFactory = new PremiumBeautyFactory();
     } else {
-        std::cerr << "Неверный тип салона красоты." << std::endl;
+        cout << "Неверный тип салона красоты." << endl;
         return 1;
     }
 
@@ -167,6 +170,11 @@ int main() {
     delete manicureService;
     delete pedicureService;
     delete salonFactory;
+
+    int a = 0;
+
+    cout << "Enter value of a -" << endl;
+    cin >> a;
 
     return 0;
 }
